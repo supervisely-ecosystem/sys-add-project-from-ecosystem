043f2364f77effeaf8a308240c34a2d91567424d
Maxim Kolomeychenko <maxim.kolomeychenko@gmail.com>
2020-08-26 16:49:32 +0300

    version done (problem with task output)

--- src/add_images_project.py
+++ src/add_images_project.py
@@ -1,24 +1,19 @@
 import os
-import json
 import supervisely_lib as sly
 
-
-context = json.loads(os.environ.get('CONTEXT', None))
-state = json.loads(os.environ.get('STATE', None))
-
-team_id = state["destination"]["teamId"]
-workspace_id = state["destination"]["workspaceId"]
-name = state["name"]
-
-github_token = context.get("githubToken", None)
-github_url = context["githubUrl"]
+team_id = os.environ['modal.state.teamId']
+workspace_id = os.environ['modal.state.workspaceId']
+project_name = os.environ['modal.state.projectName']
+ecosystem_item_git_url = os.environ['modal.state.ecosystem_item_git_url']
+ecosystem_item_version = os.environ.get('modal.state.ecosystem_item_version', "master")
+github_token = os.environ.get('modal.context.github_token', None)
 
 dest_dir = "/sly_task_data/repo"
 sly.fs.clean_dir(dest_dir)
-sly.git.download(github_url, dest_dir, github_token)
+sly.git.download(ecosystem_item_git_url, dest_dir, github_token, ecosystem_item_version, log_progress=True)
 
 api = sly.Api.from_env()
-project_name = sly.fs.get_file_name(github_url)
+project_name = sly.fs.get_file_name(ecosystem_item_git_url)
 project_id, res_project_name = sly.upload_project(dest_dir, api, workspace_id, project_name, log_progress=True)
 sly.logger.info("Project info: id={!r}, name={!r}".format(project_id, res_project_name))
 
