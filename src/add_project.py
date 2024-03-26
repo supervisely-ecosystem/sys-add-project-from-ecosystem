30499207efcb3e1d570e717e745526ac3286af0a
Almaz <79905215+almazgimaev@users.noreply.github.com>
2024-03-26 20:28:29 +0100

    SDK to 6.73.60. Update app (#11)

--- src/add_project.py
+++ src/add_project.py
@@ -9,37 +9,53 @@ from supervisely.project.pointcloud_project import upload_pointcloud_project
 from supervisely.project.pointcloud_episode_project import upload_pointcloud_episode_project
 from supervisely.app.v1.app_service import AppService
 
+
+if sly.is_development():
+    from dotenv import load_dotenv
+
+    load_dotenv("local.env")
+    load_dotenv(os.path.expanduser("~/supervisely.env"))
+
+
 my_app = AppService()
 
 
 @my_app.callback("do")
 @sly.timeit
 def do(**kwargs):
-    task_id = os.environ["TASK_ID"]
-    team_id = os.environ['modal.state.teamId']
-    workspace_id = os.environ['modal.state.workspaceId']
+    task_id = sly.env.task_id()
+    team_id = sly.env.team_id()
+    workspace_id = sly.env.workspace_id()
     project_name = os.environ.get("modal.state.projectName", "")
     project_name = project_name.replace("\\", "").replace("|", "").replace("/", "")
 
-    ecosystem_item_git_url = os.environ['modal.state.slyEcosystemItemGitUrl']
-    ecosystem_item_version = os.environ.get('modal.state.slyEcosystemItemVersion', "master")
-    ecosystem_item_id = os.environ['modal.state.slyEcosystemItemId']
+    ecosystem_item_git_url = os.environ["modal.state.slyEcosystemItemGitUrl"]
+    ecosystem_item_version = os.environ.get("modal.state.slyEcosystemItemVersion", "master")
+    ecosystem_item_id = os.environ["modal.state.slyEcosystemItemId"]
 
-    sly.logger.info("Script arguments", extra={"teamId: ": team_id,
-                                               "workspaceId: ": workspace_id,
-                                               "projectName: ": project_name,
-                                               "slyEcosystemItemId": ecosystem_item_id,
-                                               "slyEcosystemItemGitUrl: ": ecosystem_item_git_url,
-                                               "slyEcosystemItemVersion: ": ecosystem_item_version})
+    sly.logger.info(
+        "Script arguments",
+        extra={
+            "teamId: ": team_id,
+            "workspaceId: ": workspace_id,
+            "projectName: ": project_name,
+            "slyEcosystemItemId": ecosystem_item_id,
+            "slyEcosystemItemGitUrl: ": ecosystem_item_git_url,
+            "slyEcosystemItemVersion: ": ecosystem_item_version,
+        },
+    )
 
     api = sly.Api.from_env()
 
-    dest_dir = "/sly_task_data/repo"
-    sly.fs.mkdir(dest_dir)
+    # dest_dir = "/sly_task_data/repo"
+    dest_dir = my_app.data_dir
+    # sly.fs.mkdir(dest_dir)
     sly.fs.clean_dir(dest_dir)
 
-    tar_path = os.path.join(dest_dir, 'repo.tar.gz')
-    api.app.download_git_archive(ecosystem_item_id, None, ecosystem_item_version, tar_path, log_progress=True)
+    tar_path = os.path.join(dest_dir, "repo.tar.gz")
+    api.app.download_git_archive(
+        ecosystem_item_id, None, ecosystem_item_version, tar_path, log_progress=True
+    )
     with tarfile.open(tar_path) as archive:
         archive.extractall(dest_dir)
 
@@ -71,41 +87,51 @@ def do(**kwargs):
 
     project_type = sly.ProjectMeta.from_json(meta_json).project_type
     if project_type == str(sly.ProjectType.IMAGES):
-        project_id, res_project_name = sly.upload_project(dest_dir, api, workspace_id, project_name, log_progress=True)
+        project_id, res_project_name = sly.upload_project(
+            dest_dir, api, workspace_id, project_name, log_progress=True
+        )
     elif project_type == str(sly.ProjectType.VIDEOS):
-        project_id, res_project_name = sly.upload_video_project(dest_dir, api, workspace_id, project_name,
-                                                                log_progress=True)
+        project_id, res_project_name = sly.upload_video_project(
+            dest_dir, api, workspace_id, project_name, log_progress=True
+        )
     elif project_type == str(sly.ProjectType.VOLUMES):
-        project_id, res_project_name = sly.upload_volume_project(dest_dir, api, workspace_id, project_name,
-                                                                log_progress=True)
+        project_id, res_project_name = sly.upload_volume_project(
+            dest_dir, api, workspace_id, project_name, log_progress=True
+        )
     elif project_type == str(sly.ProjectType.POINT_CLOUDS):
         dest_dir = os.path.join(dest_dir, "project")
-        project_id, res_project_name = upload_pointcloud_project(dest_dir, api, workspace_id, project_name, 
-                                                                         log_progress=True)
+        project_id, res_project_name = upload_pointcloud_project(
+            dest_dir, api, workspace_id, project_name, log_progress=True
+        )
     elif project_type == str(sly.ProjectType.POINT_CLOUD_EPISODES):
         dest_dir = os.path.join(dest_dir, "project")
-        project_id, res_project_name = upload_pointcloud_episode_project(dest_dir, api, workspace_id, project_name, 
-                                                                         log_progress=True)
+        project_id, res_project_name = upload_pointcloud_episode_project(
+            dest_dir, api, workspace_id, project_name, log_progress=True
+        )
     else:
         raise NotImplementedError("Unknown project type: {}".format(project_type))
 
     sly.logger.info("Project info: id={!r}, name={!r}".format(project_id, res_project_name))
 
     # to show created project in tasks list (output column)
-    sly.logger.info('PROJECT_CREATED', extra={'event_type': sly.EventType.PROJECT_CREATED, 'project_id': project_id})
+    sly.logger.info(
+        "PROJECT_CREATED",
+        extra={"event_type": sly.EventType.PROJECT_CREATED, "project_id": project_id},
+    )
     api.task.set_output_project(task_id, project_id, res_project_name)
     my_app.stop()
 
 
 def clean_repo(extracted_path: str):
     subdirs = get_subdirs(extracted_path)
-    sly.logger.debug(f"Extracted path {extracted_path} contains following subdirectories: {subdirs}")
+    sly.logger.debug(
+        f"Extracted path {extracted_path} contains following subdirectories: {subdirs}"
+    )
 
     for subdir in subdirs:
         if subdir != "project":
             sly.fs.remove_dir(os.path.join(extracted_path, subdir))
     sly.logger.debug(f"Deleted all subdirectories except of 'project' from {extracted_path}")
-
 
 
 def main():
@@ -121,4 +147,4 @@ def main():
 
 
 if __name__ == "__main__":
-    sly.main_wrapper("main", main)
+    sly.main_wrapper("main", main, log_for_agent=False)
